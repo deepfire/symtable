f17cce6133491f18ad272b12bf0e4d540554af72
Samium Gromoff <deepfire@betelheise.feelingofgreen.ru>
2008-12-19 17:17:48 +0300

    Export NEXT-ADDR.

--- packages.lisp
+++ packages.lisp
@@ -3,5 +3,5 @@
   (:use :common-lisp :alexandria :iterate :octree-1d)
   (:export
    #:make-symtable #:symtable
-   #:add #:name #:addr #:next-name
+   #:add #:name #:addr #:next-name #:next-addr
    #:load-system-map))
